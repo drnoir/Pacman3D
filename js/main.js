b58999651f2d0eab4f56bc9afc5e297b8cd204ca
Chris Godber <chrispgodber@gmail.com>
2021-07-30 18:23:05 +0300

    sorted dynamic-body not working for collission detection on test ghost

---
+++ js/main.js
@@ -0,0 +1,27 @@
+AFRAME.registerComponent('ghost', {
+  schema: {
+    speed: {
+      type: 'number',
+      default: 1.2
+    }
+  },
+  init: function() {
+    this.target = document.querySelector("#target")
+    // this.el.components["dynamic-body"].syncToPhysics();
+  },
+  tick: function(t, dt) {
+    var currentPosition = this.el.object3D.position;
+    var distanceToTarget = currentPosition.distanceTo(target.object3D.position);
+    if (distanceToTarget < 1) return;
+    // seek player function ?
+    var targetPos = this.el.object3D.worldToLocal(target.object3D.position.clone())
+    var distance = dt*this.data.speed / 4000;
+    this.el.object3D.translateOnAxis(targetPos, distance);
+    if (this.el.components["dynamic-body"]) {
+      this.el.components["dynamic-body"].syncToPhysics()
+    }// doesn't work for box1
+
+  }
+})
+
+
