828cfe1398395baa57ab724061cef9e55509fff3
Chris Godber <chrispgodber@gmail.com>
2021-08-01 18:11:38 +0300

    adding sounds  working on changing ghost color logic - wip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 // init game variables
 let score = 0;
 let pacManAttck = false;
+let orgColourBoolean = true;
+
+const coinCount = document.querySelectorAll("coin").length;
+console.log(coinCount);
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -10,8 +14,10 @@ function MakeLarge(){
   console.log("attack on");
   const delayInMilliseconds = 10000; //1 second
   pacManAttck = true;
+  orgColourBoolean = false;
   setTimeout(function() {
     pacManAttck = false;
+    orgColourBoolean = true;
     console.log("attack off");
   }, delayInMilliseconds);
 
@@ -25,10 +31,10 @@ AFRAME.registerComponent('ghost', {
     }
   },
 
+
   init: function() {
     this.target = document.querySelector("#target")
     // this.el.components["dynamic-body"].syncToPhysics();
-
     this.el.addEventListener('collide', function (e) {
       e.detail.target.el; // Original entity (playerEl).
       e.detail.body.el; // Other entity, which playerEl touched.
@@ -37,7 +43,6 @@ AFRAME.registerComponent('ghost', {
 
       if (e.detail.body.el.className === "wall"){
         const currentPosition = this.el.object3D.position;
-        // this.el.object3D.position.set(randX, randY, z);
         this.el.object3D.position.x -+ 0.2;
         this.el.object3D.position.z -+ 0.2;
       }
@@ -70,39 +75,33 @@ AFRAME.registerComponent('ghost', {
     const distanceToTarget = currentPosition.distanceTo(target.object3D.position);
     const targetPos = this.el.object3D.worldToLocal(target.object3D.position.clone())
     const distance = dt*this.data.speed / 3000;
-    this.el.object3D.translateOnAxis(targetPos, distance);
-    // console.log(currentPosition.x );
-    // if (distanceToTarget > 6){
+    const orgColour = this.el.getAttribute('color');
 
-      // let x = currentPosition.x;
-      // let y = currentPosition.y;
-      // let z = currentPosition.z;
-      // let rand = getRandomInt(0,1)
-      // let randX=  x+rand;
-      // let randZ=  z+rand;
-      // this.el.object3D.position.set(randX, randY, z);
-     //  this.el.object3D.position.x += rand;
-     // this.el.object3D.position.z += rand;
-    //   if (this.el.components["dynamic-body"]) {
-    //     //required for dynamic body otherwise doesn't work
-    //     this.el.components["dynamic-body"].syncToPhysics()
-    //   // }
-    // }
+    this.el.object3D.translateOnAxis(targetPos, distance);
 
     if (distanceToTarget < 1) return;
     // seek player function
-    // const targetPos = this.el.object3D.worldToLocal(target.object3D.position.clone())
-    // const distance = dt*this.data.speed / 4000;
     this.el.object3D.translateOnAxis(targetPos, distance);
+
     if (this.el.components["dynamic-body"]) {
       //required for dynamic body otherwise doesn't work
       this.el.components["dynamic-body"].syncToPhysics()
     }
 
-  }
+    // color setting for ghosts when pacman is in attack mode
+    // (IN TICK SEEMS INEFFICIENT BUT CANT FIGURE ALT WAY RIGHT NOW)
 
+    if (pacManAttck === true && orgColourBoolean === false){
+      this.el.setAttribute('color','blue');
+    }
 
+    if (pacManAttck === false && orgColourBoolean === true){
+      console.log(orgColour)
+      this.el.setAttribute('color',orgColour);
+    }
+  },
 })
+
 
 AFRAME.registerComponent('pacman', {
   init: function() {
@@ -118,7 +117,7 @@ AFRAME.registerComponent('pacman', {
         box.parentNode.removeChild(winboxRemove);
         score++;
         console.log(score);
-        // document.getElementById('pickup').play();
+        document.getElementById('chomp').play();
       }
 
       if (e.detail.body.el.className === "coinLarge" ) {
@@ -128,7 +127,7 @@ AFRAME.registerComponent('pacman', {
         MakeLarge();
         score++;
         console.log(score);
-        // document.getElementById('pickup').play();
+        document.getElementById('chomp').play();
       }
 
       if (e.detail.body.el.className === "ghost" && pacManAttck === true) {
@@ -137,7 +136,7 @@ AFRAME.registerComponent('pacman', {
         let ghostRemove = e.detail.body.el;
         ghost.parentNode.removeChild(ghostRemove);
         // loseLife();
-        // document.getElementById('pickup').play();
+        document.getElementById('eatghost').play();
       }
 
     });
